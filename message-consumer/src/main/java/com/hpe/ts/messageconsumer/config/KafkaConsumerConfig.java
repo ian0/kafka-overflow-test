b877c352b0224055493fee3359d74bf8811dca59
Ian Matthews <ian.matthews@hpe.com>
2020-07-23 11:39:44 +0100

    trying to send message in call back, not working

--- message-consumer/src/main/java/com/hpe/ts/messageconsumer/config/KafkaConsumerConfig.java
+++ message-consumer/src/main/java/com/hpe/ts/messageconsumer/config/KafkaConsumerConfig.java
@@ -3,9 +3,13 @@ package com.hpe.ts.messageconsumer.config;
 import com.hpe.ts.messageconsumer.services.KafkaConsumerService;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
@@ -13,17 +17,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.retry.RetryPolicy;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,16 +45,39 @@ public class KafkaConsumerConfig {
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory(
             @Qualifier("consumerFactory") ConsumerFactory<String, String> consumerFactory,
             @Qualifier("kafkaRetryTemplate") RetryTemplate kafkaRetryTemplate,
-            @Qualifier("errorHandler") SeekToCurrentErrorHandler errorHandler) {
+            @Qualifier("overflowTemplate") KafkaTemplate<String, String> kafkaTemplate
+    ) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         factory.setRetryTemplate(kafkaRetryTemplate);
         factory.setRecoveryCallback(retryContext -> {
             ConsumerRecord consumerRecord = (ConsumerRecord) retryContext.getAttribute("record");
             logger.info("Recovery is called for message {} ", consumerRecord.value());
+            try {
+                ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send("greeting", consumerRecord.key().toString(), "an overflow message");
+                kafkaTemplate.flush();
+                send.get();
+            } catch (Exception e) {
+                logger.info(e.getMessage());
+            }
+
             return Optional.empty();
         });
         return factory;
+    }
+
+    @Bean("overflowTemplate")
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
@@ -79,7 +106,6 @@ public class KafkaConsumerConfig {
         retryTemplate.setRetryPolicy(retryPolicy);
         return retryTemplate;
     }
-
 
     @Bean("retryPolicy")
     public RetryPolicy retryPolicy() {
